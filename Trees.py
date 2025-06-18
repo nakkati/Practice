49eb5e25175418b80553629159d89647edf42c96
Nithin Akkati <nithinakkati@gmail.com>
2025-06-18 10:52:01 -0700

    BFS & DFS

--- Trees.py
+++ Trees.py
@@ -15,7 +15,7 @@ class BST:
         self.root = None
 
     def insert(self, value:int):
-        "function to insert items into BST"
+        # function to insert items into BST
         def _insert(node, value:int):
             if node is None:
                 return Node(value)
@@ -27,11 +27,30 @@ class BST:
         self.root = _insert(self.root, value)
         
 
-    def search(self, value:int, type):
-        "search based on the order"
-        
-        pass
+    def dfs_search(self, node):
+        # dfs - o(h) space complexity and o(n) time complexity
+        if not node:
+            return []
+        return [node.value] + self.dfs_search(node.left_child) + self.dfs_search(node.right_child)
+    
+    def bfs_search(self, node):
+        if not node:
+            return []
+        result = []
+        queue = [node]
 
+        while len(queue) > 0:
+            curr_node = queue.pop(0)
+            result.append(curr_node.value)
+            
+            if curr_node.left_child is not None:
+                queue.append(curr_node.left_child)
+            
+            if curr_node.right_child is not None:
+                queue.append(curr_node.right_child)
+        
+        return result
+    
     def read_tree(self, order):
         "code to traverse a tree"
         pass
@@ -41,11 +60,19 @@ class BST:
 
 def test_bst():
     tree_obj = BST()
-    tmp_list = [3,4,1,6]
+    tmp_list = [3,4,1,6, 2, 10, 20]
     for i in tmp_list:
         tree_obj.insert(i)
+    result = tree_obj.dfs_search(tree_obj.root)
+    result_bfs = tree_obj.bfs_search(tree_obj.root)
+    for i in result:
+        print("dfs:", i)
+
+    for j in result_bfs:
+        print("bfs:", j)
 
 test_bst()
+
 
 """
 Space and time complexity
