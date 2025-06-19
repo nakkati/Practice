2431a173e8de1b737e57fe1d7fd9bedd0385d3d3
nakkati <nithinakkati@gmail.com>
2025-06-19 16:00:38 -0700

    updates

--- Trees.py
+++ Trees.py
@@ -4,6 +4,7 @@ Have a class for nodes with rules for two child nodes at most.
 Have methods for insertion, deletion and search. 
 Delete method is hard and needs to be done carefully and will be implemented during self balanced trees.
 """
+"""
 class Node:
     def __init__(self, value:int):
         self.value = value
@@ -50,9 +51,53 @@ class BST:
                 queue.append(curr_node.right_child)
         
         return result
-    
+"""
 """
 segment tree - binary tree to answer range queries in o(log n) with o(n) build time and o(n) space
+useful incase where we need to do range queries on large datasets. overly complex and need to know the input size at start
+builds a segment array
+"""
+class SegmentTree_min():
+    def __init__(self, data:list):
+        self.size = len(data)
+        self.seg_tree = [float('inf')] * 4 * self.size
+        self.build(data, 0, 0, self.size - 1)
+    
+    def build(self, data, seg_pos, start_index, last_index):
+        if (start_index == last_index):
+            self.seg_tree[seg_pos] = data[start_index]
+            return
+        mid = (start_index + last_index)// 2
+        self.build(data, 2*seg_pos+1, start_index, mid)
+        self.build(data, 2*seg_pos+2, mid+1, last_index)
+        self.seg_tree[seg_pos] = min(self.seg_tree[2*seg_pos+1], self.seg_tree[2*seg_pos+2])
+
+    def query(self, q_start_pos, q_last_pos, seg_pos_root = 0, data_l_pos = 0, data_r_pos = None):
+        if data_r_pos is None:
+            data_r_pos = self.size - 1
+        # to check no overlap
+        if (q_start_pos > data_r_pos or q_last_pos < data_l_pos):
+            return float('inf')
+        # partial overlap
+        if (q_start_pos <= data_l_pos and  q_last_pos >= data_r_pos):
+            return self.seg_tree[seg_pos_root]
+        mid = (data_l_pos + data_r_pos) // 2
+        left_min = self.query(q_start_pos, q_last_pos, 2*seg_pos_root+1, data_l_pos, mid)
+        right_min = self.query(q_start_pos, q_last_pos, 2*seg_pos_root+2, mid+1, data_r_pos)
+        return min(left_min, right_min)
+
+arr_input = [-1, 0, 4, 3, 6]
+s_tree = SegmentTree_min(arr_input)
+print("minimum from index 1 to 3: ",s_tree.query(1,3))
+
+
+
+"""
+segment tree lazy propogation - binary tree to answer range queries in o(log n) with o(n) build time and o(n) space
+"""
+
+"""
+Fenwick tree
 """
 
 """
@@ -75,7 +120,7 @@ How does NCCL use trees or other libraries use them?
 """
 
 """ Test code for BST """
-
+"""
 def test_bst():
     tree_obj = BST()
     tmp_list = [3,4,1,6, 2, 10, 20]
@@ -90,3 +135,4 @@ def test_bst():
         print("bfs:", j)
 
 test_bst()
+"""
